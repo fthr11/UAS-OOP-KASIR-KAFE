019448fb0bce3e159fb9ccbee2dc840e41919235
mastertamvan12 <mastertamvan12@gmail.com>
2025-12-07 13:23:23 +0700

    baru tampilan login dan register belum database nya

---
+++ kasir-kafe/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace kasir_kafe.Controllers;   // <-- sesuaikan dengan nama namespace di HomeController
+
+public class AuthController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        // ini akan me-render Views/Auth/Login.cshtml
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string Email, string Password)
+    {
+        // TODO: nanti isi logika cek user di sini
+        // kalau gagal: return View();
+        // kalau sukses:
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost]
+    public IActionResult Register(string Name, string Email, string Password, string ConfirmPassword)
+    {
+        // TODO: simpan user ke database
+        // sementara redirect aja
+
+        return RedirectToAction("Login");
+    }
+
+}
